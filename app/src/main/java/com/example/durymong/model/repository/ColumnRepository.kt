5fab3ec2753b11ea40f6329105c69d8ea43065ef
casperjr <hyunjw0515@gmail.com>
2025-02-11 20:49:22 +0900

    [fix]: ColumnRepository에서 callback 사용하도록 함수 수정

--- app/src/main/java/com/example/durymong/model/repository/ColumnRepository.kt
+++ app/src/main/java/com/example/durymong/model/repository/ColumnRepository.kt
@@ -13,7 +13,7 @@ class ColumnRepository {
     // 여기에서 함수를 구현해서 call.enque 와 같은 작업 수행
     private val service: ColumnService = RetrofitObject.createService()
 
-    fun getColumnCategories() =
+    fun getColumnCategories(callback: (CategoryResponseDto?) -> Unit) {
         service.getColumnCategories().enqueue(object : Callback<CategoryResponseDto> {
             override fun onResponse(
                 call: Call<CategoryResponseDto>,
@@ -21,8 +21,10 @@ class ColumnRepository {
             ) {
                 if (response.isSuccessful) {
                     Log.d("successful", "카테고리 수정 성공: ")
+                    callback(response.body())
                 } else {
                     Log.d("fail", "카테고리 수정 실패: ${response.code()}")
+                    callback(null)
                 }
             }
 
@@ -30,5 +32,6 @@ class ColumnRepository {
                 Log.d("fail", "네트워크 요청 실패: ${t.message}")
             }
         })
+    }
 
 }
